da597c9446914038eb1ca8a97755d61e81575cf8
Ferdinald Dogbey <ferdinald.dogbey@amalitech.com>
2024-11-15 11:38:55 +0000

    chore: Test out creating new instance of clock object

---
+++ objectOrientedClock.js
@@ -0,0 +1,14 @@
+class Clock {
+    constructor(hours,minutes,seconds){
+    const now = new Date();
+    this.hours = now.getHours();
+    this.minutes = now.getMinutes();
+    this.seconds = now.getSeconds();
+}
+}
+const clockHours = new Clock();
+const clockMinutes = new Clock();
+const clockSeconds = new Clock();
+console.log(clockHours.hours);
+console.log(clockMinutes.minutes); 
+console.log(clockSeconds.seconds);
