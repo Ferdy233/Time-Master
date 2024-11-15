16fc166e6b0ec66e00dc252a957b6ff15192e65a
Ferdinald Dogbey <ferdinald.dogbey@amalitech.com>
2024-11-15 11:34:01 +0000

    chore: Test out time accuracy

---
+++ timeObjectBasics.js
@@ -0,0 +1,8 @@
+const currentTime = new Date();
+const hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const seconds = currentTime.getSeconds();
+
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
